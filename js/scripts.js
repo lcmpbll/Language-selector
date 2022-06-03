fac78ed18222e6bb96d592f266df4920b8255708
Liam Campbell <liam22campbell@gmail.com>
2022-06-03 09:41:40 -0700

    Answers show on submit button click and .showing hides.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".btn").click(function() {
+    $(".hidden-c").toggle();
+		$(".hidden-java").toggle();
+		$(".hidden-react").toggle();
+		$(".showing").toggle();
+  });
+});
