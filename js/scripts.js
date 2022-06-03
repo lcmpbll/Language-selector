b4201fab36a6cb300736d4d1d6fbdf784268b36a
Liam Campbell <liam22campbell@gmail.com>
2022-06-03 10:12:33 -0700

    Make values from form into integers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
+function add(subjectVal, animalVal, foodVal) {
+	return subjectVal + animalVal + foodVal;
+}
+
 $(document).ready(function() {
-  $(".btn").click(function() {
-    $(".hidden-c").toggle();
-		$(".hidden-java").toggle();
-		$(".hidden-react").toggle();
-		$(".showing").toggle();
+  $(".btn").click(function(event) {
+		event.preventDefault()
+    const subjectVal = parseInt($("#subject").val());
+		const animalVal = parseInt($("#animal").val());
+		const foodVal = parseInt($("#animal").val());
+		
+		$(".hidden-c").show();
+		$(".hidden-java").show();
+		$(".hidden-react").show();
+		$(".showing").hide();
   });
 });
