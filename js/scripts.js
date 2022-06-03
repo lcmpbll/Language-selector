a2c02680c44fc0d0aa89593a03b4a75da0ac09b0
Liam Campbell <liam22campbell@gmail.com>
2022-06-03 11:19:19 -0700

    BUGFIX animalVal now functions as a number, and all choices can be selected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function add(subjectVal, animalVal, foodVal) {
-	return subjectVal + animalVal + foodVal;
+	return subjectVal + animalVal + foodVal;	
 }
 
 $(document).ready(function() {
@@ -7,12 +7,11 @@ $(document).ready(function() {
 		event.preventDefault()
     const subjectVal = parseInt($("#subject").val());
 		const animalVal = parseInt($("#animal").val());
-		const foodVal = parseInt($("#animal").val());
+		const foodVal = parseInt($("#food").val());
 		const result = add(subjectVal, animalVal, foodVal)
-
-		if (result >= 8) {
+		if (result > 8) {
 			$(".hidden-c").show();
-		} else if (result <= 5) {
+		} else if (result > 5) {
 			$(".hidden-java").show();
 		} else {
 			$(".hidden-react").show();
